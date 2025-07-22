70a7aae400c0b527742b5a45c29c9dde567bfef5
Minaev-Aleksey <minaev.a.s@yandex.ru>
2025-07-22 13:45:45 +0300

    Добавил application.yml

--- src/main/java/com/example/GatewayAPIService/GatewayApiServiceApplication.java
+++ src/main/java/com/example/GatewayAPIService/GatewayApiServiceApplication.java
@@ -1,13 +1,25 @@
 package com.example.GatewayAPIService;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApiServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApiServiceApplication.class, args);
+	}
+
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.build());
 	}
 
 }
